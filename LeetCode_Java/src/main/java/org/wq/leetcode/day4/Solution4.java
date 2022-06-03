59e2a2186384ad70f155caf39f9a1a04bdbced45
‘wq’ <1144502582@qq.com>
2022-06-03 12:19:42 +0800

    算法练习

---
+++ LeetCode_Java/src/main/java/org/wq/leetcode/day4/Solution4.java
@@ -0,0 +1,39 @@
+package org.wq.leetcode.day4;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Solution4 {
+
+    class LRULinkHashMap<K, V> extends LinkedHashMap<K ,V>{
+        private int threshold;
+
+        public LRULinkHashMap(int threshold) {
+            super(16, 0.75f, true);
+            this.threshold = threshold;
+        }
+
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+            return size() > threshold;
+        }
+    }
+
+
+    class LRUCache {
+        private final LRULinkHashMap<Integer, Integer> lru;
+
+        public LRUCache(int capacity) {
+            lru = new LRULinkHashMap(capacity);
+        }
+
+        public int get(int key) {
+
+            return lru.get(key) == null ? -1 : lru.get(key);
+        }
+
+        public void put(int key, int value) {
+            lru.put(key, value);
+        }
+    }
+}
---
+++ LeetCode_Java/src/main/java/org/wq/leetcode/day5/HeapSort.java
@@ -0,0 +1,63 @@
+package org.wq.leetcode.day5;
+
+import java.util.Arrays;
+
+public class HeapSort {
+
+    public static void heapSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert(arr, i);
+        }
+
+        int heapSize = arr.length;
+
+        swap(arr, 0, --heapSize);
+
+        while (heapSize > 0) {
+
+            heapify(arr, 0, heapSize);
+            swap(arr, 0, --heapSize);
+        }
+    }
+
+    public static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[((index - 1) / 2)]) {
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    private static void swap(int[] arr, int index, int i) {
+        int temp = arr[index];
+        arr[index] = arr[i];
+        arr[i] = temp;
+    }
+
+    public static void heapify(int[] arr, int index, int heapSize) {
+        int left = index * 2 + 1;
+        while (left < heapSize) {
+
+            // 右孩子如果有，两个下标的值谁更大记录较大的下标
+            int largeIndex = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
+
+            // 比较子节点和父节点值大小
+            largeIndex = arr[largeIndex] > arr[index] ? largeIndex : index;
+
+            if (index == largeIndex) {
+                break;
+            }
+
+            swap(arr, index, largeIndex);
+            index = largeIndex;
+
+            left = index * 2 + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 3, 5, 6, 7, 9, 8, 2, 7};
+        heapSort(arr);
+
+        Arrays.stream(arr).forEach(System.out::print);
+    }
+}
