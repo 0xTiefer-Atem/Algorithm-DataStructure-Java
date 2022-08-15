7576690aed0b9689dcf9cc20d8112ad9d1bc6bed
‘wq’ <1144502582@qq.com>
2022-08-15 18:13:58 +0800

    算法练习

--- LeetCode_Java/src/main/java/org/wq/leetcode/Test.java
+++ LeetCode_Java/src/main/java/org/wq/leetcode/Test.java
@@ -7,6 +7,9 @@ import org.wq.leetcode.day1.Solution1;
 import org.wq.leetcode.day2.BSTIterator;
 import org.wq.leetcode.day2.Solution2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -14,87 +17,57 @@ import java.util.List;
  * @date： 2020/9/15 下午 4:56
  */
 public class Test {
-    public static void main(String[] args) {
-        Solution1 solution1 = new Solution1();
-        Solution2 solution2 = new Solution2();
 
-//        ListNode l1 = new ListNode(2);
-//        ListNode l3 = new ListNode(4);
-//        ListNode l5 = new ListNode(8);
-//        l1.next = l3;
-//        l3.next = l5;
-//
-//        ListNode l2 = new ListNode(5);
-//        ListNode l4 = new ListNode(6);
-//        ListNode l6 = new ListNode(4);
-//        l2.next = l4;
-//        l4.next = l6;
-//
-//        ListNode res = solution.addTwoNumbers(l1, l2);
-
-//        System.out.println(solution.lengthOfLastWord(" "));
-//        int[] digits = {9,8};
-//        System.out.println(Arrays.toString(solution.plusOne(digits)));
-//        System.out.println(solution.reverse(1534236469));
-//        ListNode res = solution.rotateRight(l1,2);
-//        ListNode p = res;
-//        while (p != null){
-//            System.out.print(p.val + " ");
-//            p = p.next;
-//        }
-
-//        TreeNode t1 = new TreeNode(1);
-//        TreeNode t2 = new TreeNode(2);
-//        TreeNode t3 = new TreeNode(3);
-//        TreeNode t4 = new TreeNode(4);
-//        t1.left = t4;
-//        t1.right = t3;
-//        t4.left = t2;
-//        List<Integer> res = solution.preorderTraversalNode2(t1);
-//        for (Integer i:res) {
-//            System.out.print(i + " ");
-//        }
-
-//        String str = "a good   example";
-//        System.out.println(solution2.reverseWords(str) + "====");
-//        System.out.println("example good a".equals(solution2.reverseWords(str)));
-//        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-//        System.out.println(solution2.findPeakElement(nums));
-//        solution2.rotate(nums, 3);
-
-//        TreeNode t3 = new TreeNode(3);
-//        TreeNode t7 = new TreeNode(7);
-//        TreeNode t9 = new TreeNode(9);
-//        TreeNode t15 = new TreeNode(15);
-//        TreeNode t20 = new TreeNode(20);
-//        t7.left = t3;
-//        t7.right = t15;
-//        t15.left = t9;
-//        t15.right = t20;
-//
-//        BSTIterator iterator = new BSTIterator(t7);
-//        System.out.println(iterator.next());    // 返回 3
-//        System.out.println(iterator.next());    // 返回 7
-//        System.out.println(iterator.hasNext()); // 返回 true
-//        System.out.println(iterator.next());    // 返回 9
-//        System.out.println(iterator.hasNext()); // 返回 true
-//        System.out.println(iterator.next());    // 返回 15
-//        System.out.println(iterator.hasNext()); // 返回 true
-//        System.out.println(iterator.next());    // 返回 20
-//        System.out.println(iterator.hasNext()); // 返回 false
-
-        TreeNode t1 = new TreeNode(1);
-        TreeNode t2 = new TreeNode(2);
-        TreeNode t3 = new TreeNode(3);
-        TreeNode t4 = new TreeNode(4);
-        TreeNode t5 = new TreeNode(4);
-        t1.left = t2;
-        t1.right = t3;
-        t2.right = t5;
-        t3.right = t4;
-        List<Integer> res = solution2.rightSideView(t1);
-        for (Integer i : res) {
-            System.out.print(i + " ");
+    public boolean containsNearbyDuplic(int[] nums, int k) {
+        HashMap<Integer, List<Integer>> numsMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numsMap.containsKey(nums[i])) {
+                numsMap.get(nums[i]).add(i);
+            } else {
+                List<Integer> l = new ArrayList<>();
+                l.add(i);
+                numsMap.put(nums[i], l);
+            }
         }
+
+        for (List<Integer> l : numsMap.values()) {
+            if (l.size() > 1) {
+                Collections.sort(l);
+
+                Integer i1 = l.get(0);
+                Integer i2 = l.get(l.size() - 1);
+                if (Math.abs(i1 - i2) == k) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public int longestWPI(int[] hours) {
+        int count = 0;
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < hours.length; i++) {
+            if (hours[i] > 8) {
+                count++;
+            } else {
+                count--;
+            }
+            res = Math.max(count + 1, res);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Test t = new Test();
+
+        int[] nums = {1, 2, 3, 1};
+        int k = 3;
+//        System.out.println(t.containsNearbyDuplic(nums, k));
+
+        int[] hours = {9, 9, 6, 0, 6, 6, 9};
+//        System.out.println(t.longestWPI());
     }
 }
